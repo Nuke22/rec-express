027c8a495c051dc1ce8f39c297575fecef2a747e
Nuke22 <yura.nuke22@gmail.com>
2023-06-14 20:54:31 +0300

    update 1

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongodb = require("mongodb");
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +26,60 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
+// TODO Registration TRY
+// Підключення до бази даних MongoDB
+mongoose.connect('mongodb://localhost/myapp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+      console.error('Error connecting to MongoDB:', error);
+    });
+
+// Створення моделі користувача
+const User = mongoose.model('User', {
+  first_name: String,
+  second_name: String,
+  email: String,
+  password: String
+});
+
+// Middleware для обробки JSON даних
+app.use(bodyParser.json());
+
+// Роутер для обробки реєстрації
+app.post('/create', async (req, res) => {
+  try {
+    const { first_name,second_name,email,password } = req.body;
+
+    // Перевірка, чи користувач з таким ім'ям вже існує
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: 'Користувач з таким ім\'ям вже існує' });
+    }
+
+    // Хешування пароля
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Створення нового користувача
+    const newUser = new User({ first_name,second_name,email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: 'Реєстрація пройшла успішно' });
+  } catch (error) {
+    console.error('Error during registration:', error);
+    res.status(500).json({ message: 'Помилка під час реєстрації' });
+  }
+});
+
+// // Запуск сервера
+// app.listen(3000, () => {
+//   console.log('Server started on port 3000');
+// });
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,6 +96,4 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
-// віcюлька
