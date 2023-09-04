ff7d6cd47403c3f3cb248910148583d16e2d1f71
Yurii <yura.nuke22@gmail.com>
2023-09-04 22:02:12 +0300

    databade template

--- app.js
+++ app.js
@@ -40,3 +40,36 @@ mongoose.connect('mongodb://localhost:27017/calcData', {useNewUrlParser: true, u
     .catch((error) => {
         console.error('Error connecting to MongoDB:', error);
     });
+
+
+const User = mongoose.model('User', {
+    first_name: String,
+    second_name: String,
+    email: String,
+}, 'Users');
+
+const Params = mongoose.model("Params", {
+    userId: String,
+    ParamNameId: String,
+    value: Boolean
+}, "Params")
+
+const ParamNames = mongoose.model("ParamNames", {
+    name: String,
+}, "ParamNames")
+
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+module.exports = app;
